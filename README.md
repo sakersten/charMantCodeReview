a1155f68013906ec0a7a4049fd5c9ea8f53960b2
Mark Mahoney <markm208@gmail.com>
2025-03-12 15:16:14 -0500

    Initial commit of stub code.

---
+++ README.md
@@ -0,0 +1,23 @@
+# Code Review Project
+This repository holds some starter code for an informal code review. 
+
+You and your partner will fork this repository and then clone it on your own machines. Make sure that you add me as a collaborator to the forked repo (my GitHub username is markm208).
+
+## main.cpp
+The one file that is included holds a ```main()``` function and some stubs for the required functions. 
+
+The functions need to be completed by you and your partner. You are welcome to write all of the code (including any new helper functions) in this one file, or you may add new files if you'd like. Discuss this with your partner before starting your work.
+
+All that I require is that I am able to compile and run the code from the terminal by issuing this command:
+
+```
+g++ -o  main -std=c++20 *.cpp
+```
+
+This will generate an executable named ```main``` that is created from all of the source files (```.cpp```) in the top level of the repo. I have added the name of that executable to the ```.gitignore``` file. 
+
+You are welcome to use an IDE for writing and testing your code. Recall that one way to do this is to have an IDE project folder that refers to the code in a repo. If you need help setting this up please see me ASAP.
+
+Do not delay on getting started. Working in groups tends to be more difficult than you think when first starting out. 
+
+Also, if you don't understand the requirements, please seek help immediately. I am happy to explain what I am looking for to you and your partner.
---
+++ main.cpp
@@ -0,0 +1,136 @@
+
+#include <iostream>
+using namespace std;
+
+//required function prototypes
+bool characteristic(const char numString[], int& c);
+bool mantissa(const char numString[], int& numerator, int& denominator);
+
+bool add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
+bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len); 
+
+bool multiply(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
+bool divide(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
+
+int main()
+{
+    //this c-string, or array of 8 characters, ends with the null terminating character '\0'
+    //['1', '2', '3', '.', '4', '5', '6', '\0']
+    const char number[] = "123.456"; 
+    int c, n, d;
+
+    //if both conversions from c-string to integers can take place
+    if(characteristic(number, c) && mantissa(number, n, d))
+    {
+        //do some math with c, n, and d
+        cout<<"c: "<<c<<endl;
+        cout<<"n: "<<n<<endl;
+        cout<<"d: "<<d<<endl;
+    }
+    else //at least one of the conversions failed
+    {
+        //handle the error on input
+        cout<<"Error on input"<<endl;
+    }
+
+    //room for 9 characters plus the null terminating character
+    char answer[10];
+    int c1, n1, d1;
+    int c2, n2, d2;
+
+    //initialize the values
+    c1 = 1;
+    n1 = 1;
+    d1 = 2;
+
+    c2 = 2;
+    n2 = 2;
+    d2 = 3; 
+
+    //if the c-string can hold at least the characteristic
+    if(add(c1, n1, d1, c2, n2, d2, answer, 10))
+    {
+        //display string with answer 4.1666666 (cout stops printing at the null terminating character)
+        cout<<"Answer: "<<answer<<endl;
+    }
+    else
+    {
+        //display error message
+        cout<<"Error on add"<<endl;
+    }
+
+    if(divide(c1, n1, d1, c2, n2, d2, answer, 10))
+    {
+        //display string with answer
+        cout<<"Answer: "<<answer<<endl;
+    }
+    else
+    {
+        //display error message
+        cout<<"Error on divide"<<endl;
+    }
+
+    return 0;
+} 
+//--
+bool characteristic(const char numString[], int& c)
+{
+    //hard coded return value to make the main() work
+    c = 123;
+    return true;
+}
+//--
+bool mantissa(const char numString[], int& numerator, int& denominator)
+{
+    //hard coded return value to make the main() work
+    numerator = 456;
+    denominator = 1000;
+    return true;
+}
+//--
+bool add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
+{
+    //you will have to come up with an algorithm to add the two numbers
+    //hard coded return value to make the main() work
+    result[0] = '4';
+    result[1] = '.';
+    result[2] = '1';
+    result[3] = '6';
+    result[4] = '6';
+    result[5] = '6';
+    result[6] = '6';
+    result[7] = '6';
+    result[8] = '6';
+    result[9] = '\0';
+
+    return true;
+}
+//--
+bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
+{
+    //hard coded return value to make the code compile
+    //you will have to come up with an algorithm to subtract the two numbers
+    return true;
+}
+//--
+bool multiply(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
+{
+    //hard coded return value to make the code compile
+    //you will have to come up with an algorithm to multiply the two numbers
+    return true;
+}
+//--
+bool divide(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
+{
+    //you will have to come up with an algorithm to divide the two numbers
+    //hard coded return value to make the main() work
+    result[0] = '0';
+    result[1] = '.';
+    result[2] = '5';
+    result[3] = '6';
+    result[4] = '2';
+    result[5] = '5';
+    result[6] = '\0';
+    
+    return true;
+}
