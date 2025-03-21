6e894bee31463d23c0df118f5f15f21947a24474
Sarah Kersten <117317917+sakersten@users.noreply.github.com>
2025-03-21 17:27:06 -0500

    validString Helper Function
    Helper function to check if a string is valid - addresses cases where there is more than one decimal point, a + or - in any spot other than the first position, and non-numbers.

--- main.cpp
+++ main.cpp
@@ -12,10 +12,26 @@ bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int
 bool multiply(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
 bool divide(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
 
+// characteristic and mantissa helper functions
+bool validString(const char numString[]); 
+
 int main()
 {
     //this c-string, or array of 8 characters, ends with the null terminating character '\0'
     //['1', '2', '3', '.', '4', '5', '6', '\0']
+    
+    // testing
+    cout << "0: " << validString("0") << endl; 
+    cout << "2342342: " << validString("2342342") << endl; 
+    cout << "234.234: " << validString("234.234") << endl; 
+    cout << ".234 :" << validString(".234") << endl;
+    cout << "23.: " << validString("23.") << endl; 
+    cout << "+234 :" << validString("+234") << endl;
+    cout << "-234.2: " << validString("-234.2") << endl; 
+    cout << "-234:" << validString("-234") << endl;
+    cout << "2+3: " << validString("2+3") << endl; 
+    cout << "23-: " << validString("23-") << endl; 
+
     const char number[] = "123.456"; 
     int c, n, d;
 
@@ -76,13 +92,27 @@ int main()
 bool characteristic(const char numString[], int& c)
 {
     //hard coded return value to make the main() work
-    c = 123;
+    //c = 123;
+    //return true;
+    
     return true;
 }
 //--
 bool mantissa(const char numString[], int& numerator, int& denominator)
 {
-    //hard coded return value to make the main() work
+    // hard coded return value to make the main() work
+
+    // parse string without strings
+    // spaces on either end -> skip over
+    // spaces in the middle between digits -> problem
+    // string could have decimal OR not
+    // uniary + or - start -> only FIRST character can be that 
+    // check for valid string first, if all rules are met, then actually execute the two functions
+    // successive powers of 10
+    // yes or no valid string helper
+    // start and end positions of characteristic, another for mantissa
+    // helper function for math (multiply by 10)
+
     numerator = 456;
     denominator = 1000;
     return true;
@@ -134,3 +164,72 @@ bool divide(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int l
     
     return true;
 }
+//
+
+// CHARACTERISTIC AND MANTISSA HELPER FUNCTIONS
+// helper function to check if we have a valid c-string
+bool validString(const char numString[]) 
+{
+    int i = 0; 
+
+    // check if the first character is valid (digit, +, or -)
+    if (numString[i] == '+' || numString[i] == '-')
+    {
+        i++; // increment
+    }
+
+    bool hasDecimal = false; // keep track of if there is one singular decimal point within the c-string
+
+    // check for an empty string 
+    if (numString[i] == '\0')
+    {
+        return false; 
+    }
+
+    // loop through each character in the c-string
+    while (numString[i] != '\0')
+    {
+        // check to ensure that the characters are digits, otherwise return false
+        if (numString[i] >='0' && numString[i] <= '9')
+        {
+            // continue
+        }
+
+        else if (numString[i] == '.')
+        {
+            // we cannot have return decimal occur twice
+            if (hasDecimal)
+            {
+                return false; 
+            }
+
+            // ensure the decimal is not at the beginning or end, otherwise return false
+            if (i == 0 || numString[i+1] == '\0')
+            {
+                return false; 
+            }
+        }
+
+        else if (numString[i] == '+' || numString[i] == '-')
+        {
+            return false; 
+        }
+
+        else 
+        {
+            return false; 
+        }
+        i++;
+    }
+    return true; 
+}
+
+
+// helper function to check for leading spaces
+//void skipLeadingSpaces
+
+// helper function to check 
+//void skipEndSpace
+
+// helper function to 
+//void skipMiddleSpace
